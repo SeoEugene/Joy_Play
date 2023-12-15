36e4e896d2fa806116d3fec03fcc4f16c4d3b699
coconutpalmtreeisland <65150534+SeoEugene@users.noreply.github.com>
2023-12-15 09:59:58 +0900

    first commit

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: "mongodb+srv://97eugenes:dbwlsdl12345@cluster0.umrmzhw.mongodb.net/?retryWrites=true&w=majority",
+    // 네이버 클라우드
+    access_key: "2QqJFpEM4KZxjF1THQgj",
+    secret_key: "6XILrPEELP3I5XO1fOBRRNXvDeC2CZgoHneaVpIk"
+}
